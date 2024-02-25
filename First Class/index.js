3b2b0054102e48ab0b983895d4c2f7a44c405e58
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2024-02-25 19:16:34 +0500

    Output in JavaScript

---
+++ First Class/index.js
@@ -0,0 +1,4 @@
+window.alert("Alhamdulillah, We are again going to revision of JavaScript");
+document.write("This is our first class");
+document.getElementById("h1").innerHTML = "Alhamdulillah, We are again going to revision of JavaScript";
+console.log("This is Our First Program of revision of JavaScript");
